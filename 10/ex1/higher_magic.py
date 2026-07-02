11342535cff42d6d679e5fdb3c8ccdc5e835f702
ごうだひろき <hiroz000qop@gmail.com>
2026-07-02 20:36:51 +0900

    knsg

---
+++ 10/ex1/higher_magic.py
@@ -0,0 +1,10 @@
+def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
+
+
+def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
+
+
+def conditional_caster(condition: Callable, spell: Callable) -> Callable:
+
+
+def spell_sequence(spells: list[Callable]) -> Callable:
